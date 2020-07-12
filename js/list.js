b391a785d9162af81ee7d0cf34a41750843c15e5
Amato Luca <amatoroccoluca@gmail.com>
2020-07-12 18:58:22 +0200

    a simple grocery list with js & jquery

--- js/list.js
+++ js/list.js
@@ -6,22 +6,32 @@ $(function() {
     $add_button = $('.adding-box');
     $newItemForm = $('#newItemForm');
 
+    /* 
+    if there are elements at the beginning they
+     will be shown with an initial fade 
+    */
     $('li.list-item').hide().each(function(index) {
         $(this).delay(450 * index).fadeIn(1600);
     });
 
+    /* item counter */
     function updateCount() {
         let items = $('#list > li[class!=complete]').length;
         $('#counter').text(items);
     }
     updateCount();
 
+    /* 
+    when the document is ready the form is hide. 
+    at the click on the add_button the form will be shown
+    */
     $newItemForm.hide();
     $add_button.on('click', function(e) {
         e.preventDefault();
         $newItemForm.delay(200).slideDown();
     });
 
+    /* adds the element specified in the module to the end of the list */
     $newItemForm.on('submit', function(e) {
         e.preventDefault();
         let text = $('input:text').val();
@@ -33,6 +43,11 @@ $(function() {
         
     });
 
+    /* 
+    event delegation 
+    if the item is complete it is removed 
+    else is added to the end of the list
+    */
     $list.on('click', 'li', function() {
         let $this = $(this);
         let complete = $this.hasClass('complete');
